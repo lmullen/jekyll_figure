efdffe304b4c30ccd5bc16d90b3e7e2a5d1c1f7b
Bill Parrott <william.parrott@perceptivesoftware.com>
2015-06-05 16:02:30 -0500

    enable item-specific analytics and enumeration

--- lib/jekyll_figure.rb
+++ lib/jekyll_figure.rb
@@ -15,6 +15,8 @@ module Jekyll
   # and the links will point there.
   class FigureTag < Liquid::Tag 
 
+    include Jekyll::Filters
+
     require "shellwords"
 
     def initialize(tag_name, text, tokens)
@@ -24,8 +26,6 @@ module Jekyll
     end
 
     def render(context)
-      @@fig_num += 1
-
       # If the figures directory is not defined, set dir to site root
       if defined? context.registers[:site].config["figures"]["dir"] 
         dir = context.registers[:site].config["figures"]["dir"]
@@ -33,25 +33,53 @@ module Jekyll
         dir = ""
       end
 
-      # Should we explicitly enumerate the figures?
-      enumeration = ""
-      if defined? context.registers[:site].config["figures"]["enumerate"]
-        if context.registers[:site].config["figures"]["enumerate"]
-          enumeration = "Figure #{@@fig_num}: "
-        end
-      end
-
       filename   = @text[0]
       extensions = @text[1].split(",")
+      file_slug  = slugify(filename)
       caption    = @text[2]
       img_src    = "#{dir}/#{filename}.#{extensions.first}"
 
-      # Should we include analytics links? 
-      ga = ""
-      if defined? context.registers[:site].config["figures"]["analytics"]
-        if context.registers[:site].config["figures"]["analytics"]
-          ga = %Q[onclick="ga('send', 'event', { 'eventCategory': 'Figure', 'eventAction': 'View', 'eventLabel': '#{filename}'});"]
+      enumerate  = nil
+      analytics  = nil
+
+      flags      = @text.drop(3)
+      if flags.length > 0
+        flags.each do |flag|
+          prop = flag.split('=')
+          if prop[0].downcase == 'enumerate'
+            enumerate = (prop[1].downcase == 'true')
+          elsif prop[0].downcase == 'analytics'
+            analytics = (prop[1].downcase == 'true')
+          end
         end
+      end
+
+      # Should we explicitly enumerate the figures?
+      if enumerate.nil?
+        if defined? context.registers[:site].config["figures"]["enumerate"] and context.registers[:site].config["figures"]["enumerate"]
+          enumerate = true
+        end
+      end
+
+      enumeration = ""
+      if enumerate
+        @@fig_num += 1
+        enumeration = "Figure #{@@fig_num}: "
+        figure_id   = "figure-#{@@fig_num}"
+      else
+        figure_id   = "figure-#{file_slug}"
+      end
+
+      # Should we include analytics links? 
+      if analytics.nil?
+        if defined? context.registers[:site].config["figures"]["analytics"] and context.registers[:site].config["figures"]["analytics"]
+          analytics = true
+        end
+      end
+
+      ga = ""
+      if analytics
+        ga = %Q[onclick="ga('send', 'event', { 'eventCategory': 'Figure', 'eventAction': 'View', 'eventLabel': '#{filename}'});"]
       end
 
       downloads  = proc {
@@ -63,12 +91,12 @@ module Jekyll
       }
 
 
-      "<figure id='figure-#{@@fig_num}'>"                                +
-      "<a #{ga} href='#{img_src}'>"                                      +
-      "<img src='#{img_src}' alt='#{caption}'></a>"                      +
-      "<figcaption>#{enumeration}"                                       +
-      "#{caption} [#{downloads.call}]"                                   +
-      "</figcaption>"                                                    +
+      "<figure id='#{figure_id}'>"                  +
+      "<a #{ga} href='#{img_src}'>"                 +
+      "<img src='#{img_src}' alt='#{caption}'></a>" +
+      "<figcaption>#{enumeration}"                  +
+      "#{caption} [#{downloads.call}]"              +
+      "</figcaption>"                               +
       "</figure>"
 
     end
@@ -77,4 +105,3 @@ module Jekyll
 end
 
 Liquid::Template.register_tag('figure', Jekyll::FigureTag)
-
